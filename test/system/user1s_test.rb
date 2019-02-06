c99b6380d4718507a8e42fb8ef7218e6e6bbb98c
hlturner1660 <turner_hannah1@columbusstate.edu>
2019-02-06 23:33:04 +0000

    second commit

---
+++ test/system/user1s_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class User1sTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit user1s_url
+  #
+  #   assert_selector "h1", text: "User1"
+  # end
+end
